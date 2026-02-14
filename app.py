9f0691874ecf0e628c435bbaa81a17b1ad94bd26
naveenkumarj407-png <naveenkumarj407@gmail.com>
2026-02-14 12:32:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+print("STARTING APP...")
+from flask import Flask, render_template, request 
+import mysql.connector
+
+app = Flask(__name__)
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="tiger",
+    database="portfolio_db",
+    use_pure=True   # ⭐ IMPORTANT FIX
+)
+
+cursor = db.cursor(dictionary=True)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/contact", methods=["POST"])
+def contact():
+    name = request.form["name"]
+    email = request.form["email"]
+    message = request.form["message"]
+
+    sql = "INSERT INTO messages (name, email, message) VALUES (%s, %s, %s)"
+    cursor.execute(sql, (name, email, message))
+    db.commit()
+
+    return "Message saved successfully"
+
+@app.route("/messages")
+def messages():
+    cursor.execute("SELECT * FROM messages")
+    data = cursor.fetchall()
+    return render_template("messages.html", messages=data)
+
+if __name__ == "__main__":
+    print("RUNNING FLASK")  
+    app.run(debug=True)
